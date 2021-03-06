8c1f895917c6719255ca460de71d7039a6e6e4fd
3onier <30025379+3onier@users.noreply.github.com>
2021-03-06 13:02:05 +0100

    Added generic error message

--- config/default_messages.py
+++ config/default_messages.py
@@ -7,3 +7,10 @@ ERROR_PERMISSION_EMBED = Embed(
                 description="You are not permitted to run this command",
                 color=colors.COLOR_ERROR
             )
+
+ERROR_GENERIC_EMBED = Embed(
+                type="rich",
+                title="Error",
+                description="Something went wrong",
+                color=colors.COLOR_ERROR
+            )
