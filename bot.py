50ce784869c1f89942d0c7fd12fa9c3a995706e7
3onier <30025379+3onier@users.noreply.github.com>
2021-03-06 12:33:18 +0100

    Added Basic Emoji Avatar features

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 
 from cogs.VoiceManager import VoiceManager
+from cogs.EmojiManager import EmojiManager
 
 bot = commands.Bot(command_prefix="$")
 
 bot.add_cog(VoiceManager(bot))
+bot.add_cog(EmojiManager(bot))
 
 @bot.command("ping")
 async def ping(ctx):
